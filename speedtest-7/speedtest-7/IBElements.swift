11f3d4eb9bf08554bb959180f9d01e01240c333a
Omar <omsi96@users.noreply.github.com>
2020-10-07 16:11:50 +0300

    FIX THE CRASH!

--- speedtest-7/speedtest-7/IBElements.swift
+++ speedtest-7/speedtest-7/IBElements.swift
@@ -15,8 +15,9 @@ import UIKit
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
+    
     @IBInspectable
     var cornerRadius: CGFloat {
         get {
@@ -100,7 +101,7 @@ import UIKit
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     @IBInspectable
     var cornerRadius: CGFloat {
@@ -188,7 +189,7 @@ import UIKit
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     @IBInspectable
     var cornerRadius: CGFloat {
